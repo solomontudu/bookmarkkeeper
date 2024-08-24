dee107de66dd5149a2d1d8bb1373f6c5024276d9
solomontudu <tukintudu@gmail.com>
2024-08-24 21:22:53 +0530

    finished the UI designing, the add bookmark modal proceedint to the javascript side of the app

---
+++ script.js
@@ -0,0 +1,16 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const wesiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("wesite-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+
+// show modal, focus on input
+function showModal() {
+  modal.classList.add("show-modal");
+  wesiteNameEl.focus();
+}
+
+// modal event listeners
+modalShow.addEventListener("click", showModal);
