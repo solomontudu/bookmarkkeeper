18db5139f7b3990a2dda32b9d81acc8edb5677d0
solomontudu <tukintudu@gmail.com>
2024-08-24 21:31:07 +0530

    added the functionality to be opened and closed, including the functionlity when the overlay is clicked the modal is closed

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function showModal() {
 
 // modal event listeners
 modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
+window.addEventListener("click", (e) => {
+  e.target === modal ? modal.classList.remove("show-modal") : null;
+});
