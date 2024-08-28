d6b1265d9098469ae55bd620e2f6d88503b6289e
solomontudu <tukintudu@gmail.com>
2024-08-28 20:51:01 +0530

    added the functionality of persistance by storing the bookmarks in the local storage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const wesiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 // show modal, focus on input
 function showModal() {
   modal.classList.add("show-modal");
@@ -22,24 +24,41 @@ window.addEventListener("click", (e) => {
 });
 
 // validate form
-function validate(nameValue,urlValue){
+function validate(nameValue, urlValue) {
   const expression =
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-    const regex = new RegExp(expression);
-    if(!nameValue || !urlValue){
-      alert('Please submit values for both fields.');
-      return false;
-    }
-    if(urlValue.match(regex)){
-      alert('match');
-    }
-    if(!urlValue.match(regex)){
-      alert('Please provide a valid web address');
-      return false;
-    }
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+  // if (urlValue.match(regex)) {
+  //   alert("match");
+  // }
+  if (!urlValue.match(regex)) {
+    alert("Please provide a valid web address");
+    return false;
+  }
 
-    // Valid 
-    return true;
+  // Valid
+  return true;
+}
+
+// fetch bookmarks
+function fetchBookmarks() {
+  // get bookmarks for localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "Google",
+        url: "https://google.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
 }
 
 // handle data from form
@@ -47,15 +66,28 @@ function storeBookmark(e) {
   e.preventDefault();
   const nameValue = wesiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if(!urlValue.includes('http://', 'https://')) {
+  if (!urlValue.includes("http://", "https://")) {
     urlValue = `https://${urlValue}`;
   }
-  
+
   if (!validate(nameValue, urlValue)) {
     return false;
   }
-  console.log(nameValue, urlValue);
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  wesiteNameEl.focus();
 }
 
 // event listner
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// onload, fetch bookmarks
+fetchBookmarks();
